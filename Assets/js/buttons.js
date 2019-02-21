bfae048ab7e81c1358cd21517a7f49d3f7cb905b
Elleonors <Wauquier.Teddy@gmail.com>
2019-02-21 16:21:02 +0100

    correcting bootstrap 4 problems

---
+++ Assets/js/buttons.js
@@ -0,0 +1,11 @@
+//buttons
+
+document.getElementById("binche").addEventListener("click", function() {
+  document.getElementById("menu").innerHTML = "voici l'emplacement et les horaires pour Binche"
+});
+document.getElementById("la-louviere").addEventListener("click", function() {
+  document.getElementById("menu").innerHTML = "voici l'emplacement et les horaires pour La Louviere";
+});
+document.getElementById("mons").addEventListener("click", function() {
+  document.getElementById("menu").innerHTML = "voici l'emplacement et les horaires pour Mons";
+});
